500e67a8ffaed2e0a1cd6cf148a0dd69b58c871f
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-03-03 22:50:49 +0900

    Lesoon15課題

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,25 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InputController {
+
+	@GetMapping("/input")
+	public String getInput() {
+		// input.htmlに画面遷移
+		return "input";
+	}
+	//model.addAttributeを使ってpreviousをModelに登録
+	@PostMapping("/input/{previous}")
+	public String getOutput(@RequestParam("previous")String previous,Model model) {
+		//フォームから送信されてきた値をModelに登録
+		model.addAttribute("previous", previous);
+		// confirm.htmlに画面遷移
+		return "input";
+	}
+}
