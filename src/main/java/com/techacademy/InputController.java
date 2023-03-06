9657c724b63c66560e94f7457442c83e0564de2f
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-03-06 21:11:06 +0900

    output

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,10 +1,7 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
@@ -12,14 +9,6 @@ public class InputController {
 	@GetMapping("/input")
 	public String getInput() {
 		// input.htmlに画面遷移
-		return "input";
-	}
-	//model.addAttributeを使ってpreviousをModelに登録
-	@GetMapping("/input/{previous}/")
-	public String getOutput(@RequestParam("previous")String previous,Model model) {
-		//フォームから送信されてきた値をModelに登録
-		model.addAttribute("previous", previous);
-		// index.htmlに画面遷移
 		return "input";
 	}
 }
